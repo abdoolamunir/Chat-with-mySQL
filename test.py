9e3ed5e95c8f9c42e17b8e9104669e8bcd0aea2a
Abdullah Munir <152431896+abdoolamunir@users.noreply.github.com>
2024-07-16 20:41:32 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+template = """Based on the table schema below, write a SQL query that would answer the user's question:
+{schema}
+
+Question: {question}
+SQL Query:"""
+prompt = ChatPromptTemplate.from_template(template)
+
+print("Import successful and template created!")
